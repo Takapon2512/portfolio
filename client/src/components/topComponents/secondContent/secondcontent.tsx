7e652f0f8293203fb64d08aab032f175f5c241c1
Takayuki0924 <takapon0924@icloud.com>
2023-09-15 21:11:17 +0900

    2023-09-15

--- client/src/components/topComponents/secondContent/secondcontent.tsx
+++ client/src/components/topComponents/secondContent/secondcontent.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
+import gsap from "gsap";
 
 //MUI
 import { 
@@ -7,7 +8,7 @@ import {
     Typography,
     Link,
     List,
-    ListItem
+    Paper
 } from '@mui/material';
 
 //MUIIcon
@@ -43,7 +44,32 @@ const SecondContent = () => {
             icon: <Image className={styles.secC_image} width={72} height={72} src="/images/pc_man.png" alt="実際は暗記できていないのにアプリでは暗記済みになっている" />,
             text: "実際は暗記できていないのにアプリでは暗記済みになっている..."
         }
-    ]
+    ];
+
+    useEffect(() => {
+
+        const onScroll = () => {
+            const challengeEl1 = document.getElementById("challenge1"); 
+            const challengeEl2 = document.getElementById("challenge2"); 
+            const challengeEl3 = document.getElementById("challenge3");
+
+            if (!challengeEl1 || !challengeEl2 || !challengeEl3) return;
+            
+            const screenHeight = window.innerHeight || document.documentElement.clientHeight;
+            const scrollPosition = window.scrollY;
+
+            if (scrollPosition > screenHeight / 2) {
+                gsap.to(challengeEl1, { opacity: 1, duration: 1, x: 0 });
+                gsap.to(challengeEl2, { opacity: 1, duration: 1, delay: 0.2, x: 0 });
+                gsap.to(challengeEl3, { opacity: 1, duration: 1, delay: 0.4, x: 0 });
+
+                window.removeEventListener('scroll', onScroll);
+            };
+        };
+
+        window.addEventListener('scroll', onScroll);
+
+    }, []);
 
     return (
         <>
@@ -58,7 +84,12 @@ const SecondContent = () => {
                 <List className={styles.secC_Lists}>
                     {
                         challenges.map((item: ChallengesType, index: number) => (
-                            <ListItem key={index} className={styles.secC_Item}>
+                            <Paper 
+                            key={index} 
+                            className={styles.secC_Item} 
+                            id={`challenge${index + 1}`} 
+                            style={{ opacity: 0, transform: "translateX(-2000px)" }}
+                            >
                                 <Typography className={`${notoSansJP.className} ${styles.secC_case}`}>
                                     {`Case 0${index + 1}`}
                                 </Typography>
@@ -66,7 +97,7 @@ const SecondContent = () => {
                                 <Typography className={`${notoSansJP.className} ${styles.secC_Text}`}>
                                     { item.text }
                                 </Typography>
-                            </ListItem>
+                            </Paper>
                         ))
                     }
                 </List>
--- client/src/components/topComponents/thirdContent/thirdcontent.tsx
+++ client/src/components/topComponents/thirdContent/thirdcontent.tsx
@@ -18,31 +18,47 @@ import styles from './thirdcontent.module.scss';
 import eStudy01 from '../../../../public/e-study_img01.jpg';
 
 const ThirdContent = () => {
+    //機能のタイトル
+    const functions = [];
+
+    //紹介画像のURL
+
+    //機能の詳細
 
     return (
         <>
         <Box className={styles.thiC}>
             <Box className={styles.thiC_container}>
-                <Image 
-                className={styles.thiC_image}
-                src={eStudy01}
-                width={1280}
-                height={905}
-                alt='苦手な暗記を得意に'
-                />
                 <Typography
                 variant="h3"
-                className={`${notoSansJP.className} ${styles.thiC_subtitle}`}
+                className={`${notoSansJP.className} ${styles.thiC_title}`}
                 >
-                    確認テストで定着度UP
+                    サポート機能で<br />
+                    これらの課題を解決します！
                 </Typography>
-                <Typography
-                className={`${notoSansJP.className} ${styles.thiC_subDescription}`}
-                >
-                    暗記カードで覚えた単語をすぐにテストすることで、定着させます。
-                    <br></br>
-                    満点を取れるまで続くため、覚え漏れの心配はありません。
-                </Typography>
+                <Box className={styles.thiC_ImageContainer}>
+                    <Image width={540} height={434} src="/images/memorization1.png" alt="暗記機能" className={styles.thiC_Image} />
+                </Box>
+                <Box className={styles.thiC_ImageContainerBack}></Box>
+                <Box className={styles.thiC_TextBoxRight}>
+                    <Box className={styles.thiC_TextContainer}>
+                        <Box className={styles.thiC_TextBox}>
+                            <Typography className={styles.thiC_textTitle}>
+                                暗記カードの機能
+                            </Typography>
+                            <Typography className={styles.thiC_description}>
+                                暗記カードでは、出題対象の単語を覚えるまで
+                                <br />
+                                何度でも後回しできます。
+                                <br />
+                                操作は簡単で、覚えたと思ったら「覚えた！」ボタンを、
+                                そうでない場合は「もう一度」ボタンで後で覚え直すことができます。
+                                <br />
+                                また、アプリに登録された日本語訳は、カードをクリックすると表示されます。
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Box>
             </Box>
         </Box>
         </>
